782a8f7d412f25e482e2bb37edbe342c8c578ed4
Jeremías Cortés <jeremiasacortes@gmail.com>
2026-07-26 13:08:06 +0200

    Changed: Propiedades en `Product` (`category_id` y `barCode`) para mejorar consistencia y permitir valores nulos
    Signed-off-by: Jeremías Cortés <jeremiasacortes@gmail.com>

--- src/main/kotlin/com/jeremiascortes/LoQueTengo/backend/entity/Product.kt
+++ src/main/kotlin/com/jeremiascortes/LoQueTengo/backend/entity/Product.kt
@@ -15,11 +15,11 @@ class Product(
     var name: String,
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category", nullable = false)
+    @JoinColumn(name = "category_id", nullable = false)
     var category: Category,
 
-    @Column(nullable = false, length = 255)
-    var barCode: String,
+    @Column(nullable = true, length = 255)
+    var barCode: String? = null,
 
     @Column(nullable = false, name = "user_id")
     val userId: UUID
