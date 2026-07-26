c4d6c172cf0e743ae1f667262408bb09d626e586
Jeremías Cortés <jeremiasacortes@gmail.com>
2026-07-26 12:06:48 +0200

    Added: DTO `ProductRequest` con validaciones para los campos `name` y `category`
    Signed-off-by: Jeremías Cortés <jeremiasacortes@gmail.com>

---
+++ src/main/kotlin/com/jeremiascortes/LoQueTengo/backend/dto/request/ProductRequest.kt
@@ -0,0 +1,16 @@
+package com.jeremiascortes.LoQueTengo.backend.dto.request
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import java.util.*
+
+data class ProductRequest(
+    @field:NotBlank(message = "El nombre del producto es obligatorio")
+    @field:Size(max = 64, message = "El campo nombre, máximo debe contener 64 caracteres")
+    var name: String,
+
+    @field:NotBlank(message = "La categoria de producto es obligatorio")
+    var category: UUID,
+
+    var bar_code: String? = null
+)
