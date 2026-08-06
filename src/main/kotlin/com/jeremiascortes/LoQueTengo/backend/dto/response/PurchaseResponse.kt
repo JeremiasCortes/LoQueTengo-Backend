e75ab44d3e87b658077f95d53a77d6f6c21a25f7
Jeremías Cortés <jeremiasacortes@gmail.com>
2026-08-06 13:47:59 +0200

    Added: Método estático `fromEntity` en `PurchaseResponse` para mapear desde la entidad `Purchase`
    Signed-off-by: Jeremías Cortés <jeremiasacortes@gmail.com>

--- src/main/kotlin/com/jeremiascortes/LoQueTengo/backend/dto/response/PurchaseResponse.kt
+++ src/main/kotlin/com/jeremiascortes/LoQueTengo/backend/dto/response/PurchaseResponse.kt
@@ -1,9 +1,18 @@
 package com.jeremiascortes.LoQueTengo.backend.dto.response
 
+import com.jeremiascortes.LoQueTengo.backend.entity.Purchase
 import java.math.BigDecimal
 import java.time.Instant
 
 data class PurchaseResponse(
     val date: Instant,
     val total: BigDecimal
-)
+) {
+    companion object {
+        fun fromEntity(entity: Purchase): PurchaseResponse =
+            PurchaseResponse(
+                date = entity.date,
+                total = entity.total
+            )
+    }
+}
