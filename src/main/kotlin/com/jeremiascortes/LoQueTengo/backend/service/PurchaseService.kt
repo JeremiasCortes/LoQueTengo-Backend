d4f8bc69395f9b349fc190e32c8b8889e132ccb8
Jeremías Cortés <jeremiasacortes@gmail.com>
2026-08-06 13:48:41 +0200

    Added: Métodos CRUD en `PurchaseService` para gestión de compras, incluyendo consultas por ID y fecha
    Signed-off-by: Jeremías Cortés <jeremiasacortes@gmail.com>

--- src/main/kotlin/com/jeremiascortes/LoQueTengo/backend/service/PurchaseService.kt
+++ src/main/kotlin/com/jeremiascortes/LoQueTengo/backend/service/PurchaseService.kt
@@ -1,18 +1,62 @@
 package com.jeremiascortes.LoQueTengo.backend.service
 
+import com.jeremiascortes.LoQueTengo.backend.dto.request.PurchaseRequest
 import com.jeremiascortes.LoQueTengo.backend.entity.Purchase
-import com.jeremiascortes.LoQueTengo.backend.repository.ProductRepository
 import com.jeremiascortes.LoQueTengo.backend.repository.PurchaseRepository
 import com.jeremiascortes.LoQueTengo.backend.security.SecurityContext
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.Instant
+import java.util.*
 
 @Service
 class PurchaseService(
     private val purchaseRepository: PurchaseRepository,
     securityContext: SecurityContext
 ): BaseService(securityContext) {
+
+    @Transactional(readOnly = true)
     fun getAll(): List<Purchase> =
         purchaseRepository.findAllByUserId(currentUserId())
 
-    fun getById(): Purchase = TODO()
+    @Transactional(readOnly = true)
+    fun getById(id: UUID): Purchase =
+        purchaseRepository.findPurchaseByIdAndUserId(
+            id = id,
+            userId = currentUserId()
+        ) ?: throw IllegalArgumentException("Compra no encontrada")
+
+    @Transactional(readOnly = true)
+    fun getByDate(date: Instant): List<Purchase> =
+        purchaseRepository.findPurchaseByDateAndUserId(
+            date = date,
+            userId = currentUserId()
+        )
+
+    @Transactional
+    fun create(request: PurchaseRequest): Purchase {
+        val purchase = Purchase(
+            date = request.date,
+            total = request.total,
+            userId = currentUserId()
+        )
+
+        return purchaseRepository.save(purchase)
+    }
+
+    @Transactional
+    fun update(
+        request: PurchaseRequest,
+        id: UUID
+    ): Purchase {
+        val purchase = getById(id)
+
+        purchase.date = request.date
+        purchase.total = request.total
+
+        return purchaseRepository.save(purchase)
+    }
+
+    @Transactional
+    fun delete(id: UUID) = getById(id).let { purchase -> purchaseRepository.delete(purchase) }
 }
